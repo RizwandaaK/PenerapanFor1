1ffb78f3bdacd57ac043e34f302951a8d8579184
RizwandaaK <rizwanda_keysha_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 21:49:59 +0700

    PenerapanFor1

---
+++ src/penerapanfor1/PenerapanFor1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penerapanfor1;
+
+/**
+ *
+ * @author rizwa
+ */
+public class PenerapanFor1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i=1 ; i<=4 ; i++){
+            for (int j =1; j <= 4; j++) {
+                if (i==1 || i==4 || j==1 || j==4) {
+                    System.out.print("@");
+                }else{
+                    System.out.print(" ");
+            }
+    }System.out.println(" ");
+}
+}
+}
